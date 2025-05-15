18cbf8a1f8fc3623c9898934ae5b9505c2fdb0bc
Mattia <mcolucci@studenti.apuliadigitalmaker.it>
2025-05-15 10:47:18 +0200

    start project

---
+++ backend/troubleticketing/src/main/java/com/sincon/troubleticketing/TroubleticketingApplication.java
@@ -0,0 +1,13 @@
+package com.sincon.troubleticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroubleticketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroubleticketingApplication.class, args);
+	}
+
+}
---
+++ backend/troubleticketing/src/test/java/com/sincon/troubleticketing/TroubleticketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sincon.troubleticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroubleticketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
